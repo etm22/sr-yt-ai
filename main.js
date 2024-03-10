a48066b08ade233e30753c4d4639d3edb62df7d9
etm22 <easytubemastery@gmail.com>
2024-03-10 12:29:46 +0400

    create tts

--- main.js
+++ main.js
@@ -3,6 +3,16 @@ const fs = require("fs/promises");
 
 
 (async () => {
-    const { audio } = await convertTTS("hello world", "en_us_006")
-    await fs.writeFile(`audio.wav`, audio);
+    const prompts_json = JSON.parse(await fs.readFile("./data/prompts.json", "utf-8"))
+    const selected_prompt = prompts_json.find(prompt => !prompt.hasOwnProperty("used"))
+
+    // create tts + alignment
+    const text = `I asked artificial intelligence to create an image of ${selected_prompt.user_prompt}`
+    const { audio, payload } = await convertTTS(text, "en_us_006")
+    await fs.writeFile(`output/audio.wav`, audio);
+    await fs.writeFile("output/alignment.json", JSON.stringify(payload))
+
+
+    // TODO: set used
+
 })()
